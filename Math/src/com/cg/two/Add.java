6677a08bb2ada02f0937891d8f13e584905804b6
chetanibm <chetangupta@in.ibm.com>
2015-09-21 03:15:11 +0530

    Release update

--- Math/src/com/cg/two/Add.java
+++ Math/src/com/cg/two/Add.java
@@ -10,7 +10,7 @@ package com.cg.two;
 public class Add {
 	public int add (int val1, int val2) {
 		
-		System.out.println("Math -> Release 7");
+		System.out.println("Math -> Release 8");
 		if (val1 >100) {
 			throw new RuntimeException("Invalid Percentage being added");
 		}
